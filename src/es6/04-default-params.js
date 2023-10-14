fad68fdf33466076e7d93aa73a9fd02b0e1c564a
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-14 12:01:12 -0600

    Mejor manera de asignar Parametros por defecto en funciones

---
+++ src/es6/04-default-params.js
@@ -0,0 +1,18 @@
+function newUser(name, age, country) {
+    var name = name || 'Oscar';
+    var age = age || 34;
+    var country = country || 'MX';
+    console.log(name, age, country);
+}
+
+newUser();
+newUser('David', 15, 'CO');
+
+// Nueva manera de asignar parametros por Default
+
+function newAdmin(name = 'Oscar', age = 32, country = 'CL') {
+    console.log(name, age, country);
+}
+
+newAdmin();
+newAdmin('Ana', 28, 'PE');
