d2f72819f9502df341bdd1e61d32d3b54f35b0f1
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-14 12:47:11 -0600

    ES6: spread operator, rest

--- src/es6/05-rest-spread.js
+++ src/es6/05-rest-spread.js
@@ -10,3 +10,11 @@ let user = { userName: 'Oscar', age: 34, };
 let { userName, age } = user;
 console.log(userName, user.age);
 
+// spread operator ...
+// Los tres puntos ... antes de person se utilizan para realizar una operación llamada "spread" o "propagación". En este contexto, lo que hace es copiar todas las propiedades del objeto person en el nuevo objeto data, junto con la propiedad country.
+
+let person = { name: 'oscar', age: 28 };
+let country = 'MX';
+
+let data = { ...person, country };
+console.log(data);
