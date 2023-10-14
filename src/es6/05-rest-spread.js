a13e5bc9e9301f24b9e7f2e679f47df136e2fad0
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-14 12:32:47 -0600

    ES6: asignación de desestructuración

---
+++ src/es6/05-rest-spread.js
@@ -0,0 +1,12 @@
+// arrays destructuring
+
+let fruits = ['Apple', 'Banana'];
+let [a, b] = fruits;
+console.log(a, fruits[1]);
+
+// object destructuring
+
+let user = { userName: 'Oscar', age: 34, };
+let { userName, age } = user;
+console.log(userName, user.age);
+
