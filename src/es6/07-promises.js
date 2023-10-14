d3fecf8c69e58df5a4248e907b726b3b7f884bbb
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-14 13:43:43 -0600

    Uso de promesas

---
+++ src/es6/07-promises.js
@@ -0,0 +1,13 @@
+const anotherFuncion = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve('Heyyy!!');
+        } else {
+            reject('Buuuuu!!');
+        }
+    })
+}
+
+anotherFuncion()
+    .then(response => console.log(response))
+    .catch(err => console.log(err));
