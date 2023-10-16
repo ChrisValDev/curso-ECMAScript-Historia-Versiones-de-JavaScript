3d2d2d3ba6607be8f0df2e4edeb7bb53c23f7fd8
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-16 11:38:17 -0600

    Uso de generator

---
+++ src/es6/10-generator.js
@@ -0,0 +1,14 @@
+function* iterate(array) {// El generator de distingue por el signo asterisco (*) despues de la palabra function.
+    for (let value of array) {
+        yield value;
+    }
+}
+
+const it = iterate(['Oscar', 'David', 'Ana', 'Ulises', 'Jennifer']);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
