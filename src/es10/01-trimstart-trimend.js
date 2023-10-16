d8770552328f6bc42542f6f6f9821287803c2dae
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-16 14:35:55 -0600

    metodos .trimStart y .trimEnd

---
+++ src/es10/01-trimstart-trimend.js
@@ -0,0 +1,5 @@
+const hello = '           Hello world!                ';
+console.log(hello.trimStart());
+console.log(hello.trimEnd());
+
+// Los métodos trimStart() y trimEnd() son métodos de las cadenas de texto en JavaScript que se utilizan para eliminar espacios en blanco (como espacios, tabulaciones y saltos de línea) al principio o al final de una cadena de texto, respectivamente.
