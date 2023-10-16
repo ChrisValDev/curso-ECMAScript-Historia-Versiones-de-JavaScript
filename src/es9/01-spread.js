e42bed3ca9aa4e59e6feb4c25c3e5326d44c0f72
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-16 13:44:03 -0600

    expresiones regulares

---
+++ src/es9/01-spread.js
@@ -0,0 +1,11 @@
+// Admite el uso de read y spread con objetos.
+
+const user = {
+    username: 'gndx',
+    age: 34,
+    country: 'CO',
+};
+
+const {username, ...values} = user;
+console.log(username);
+console.log(values);
