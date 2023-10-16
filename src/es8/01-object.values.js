b5dc6382ab0adf87563bd71af40e9f794a2ebef3
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-16 12:56:24 -0600

    Metodo Object.values

---
+++ src/es8/01-object.values.js
@@ -0,0 +1,7 @@
+const countries = { MX: 'Mexico', CO: 'Colombia', CL: 'Chile', PE: 'Peru'};
+
+console.log(Object.values(countries)); //Solo muestra los valores en un array.
+
+//Esto muestra consola
+[ 'Mexico', 'Colombia', 'Chile', 'Peru' ]
+
