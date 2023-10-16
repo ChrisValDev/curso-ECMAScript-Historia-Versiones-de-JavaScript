b2d00c606e00e473306007c270d592aa1eb937fb
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-16 17:58:26 -0600

    array.at

---
+++ src/es13/00-at.js
@@ -0,0 +1,7 @@
+const array = ["one", "two", "three", "four", "five", "six"];
+
+console.log(array[array.length - 1]);
+
+console.log(array.at(3));
+
+// La función Array.at() se utiliza para acceder a un elemento en una posición específica del array, incluso si el índice es negativo. Aquí tienes una explicación del código que proporcionaste:
