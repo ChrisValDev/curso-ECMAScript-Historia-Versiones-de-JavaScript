7c77bccc2fb163197b461d19694d9568be6f2787
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-16 17:27:49 -0600

    numeric-separators

---
+++ src/es12/00-numeric-separators.js
@@ -0,0 +1,4 @@
+const value = 100_000_000_000;
+console.log(value); // 100000000000 Se muestra en consola
+ 
+//El guion bajo en numero extensos ayuda a que podamos visualizar de manera clara de cuantos numero se compone el valor.
