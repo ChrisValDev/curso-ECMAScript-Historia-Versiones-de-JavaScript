a65741125a6533a42c0426be279fd28c0ca79e0f
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-16 17:31:07 -0600

    Replace all

---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,7 @@
+const string = "JavaScript es un maravilloso lenguaje de programacion";
+
+const replaceString = string.replace("JavaScript", "TypeScript");
+
+console.log(replaceString);
+
+// Metodod para remplazar una parte en especifica de textos.
