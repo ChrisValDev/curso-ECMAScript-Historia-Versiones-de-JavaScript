f93cd19df161fa5ce795c20c51da08c82a98ebdc
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-16 12:44:46 -0600

    Metodo en array includes

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,9 @@
+let numbers = [1, 2, 5, 32, 76, 8, 4, 3];
+
+console.log(numbers.includes(90));
+
+const list = ['Oscar', 'Pedro', 'Federico']
+
+console.log(list.includes('Oscar'));
+
+// Metodo includes evuelve true o false
