544b496699358cbe99fe25ffc426060195365a86
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-16 12:52:10 -0600

    Metodo Object.entries

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,10 @@
+const countries = { MX: 'Mexico', CO: 'Colombia', CL: 'Chile', PE: 'Peru' };
+console.log(Object.entries(countries)); //Transforma el objeto en una array con varios arrays.
+
+// Esto muestra consola.
+[
+    [ 'MX', 'Mexico' ],
+    [ 'CO', 'Colombia' ],
+    [ 'CL', 'Chile' ],
+    [ 'PE', 'Peru' ]
+  ]
